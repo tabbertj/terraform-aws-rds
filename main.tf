fe4c2cc8c6ca03e52efad186fe51eb993e17dcc1
Dipesh Arora <dipesharora@msn.com>
2023-07-18 14:27:17 -0400

    Update Postgres engine version

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_db_instance" "demo" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "14.1"
+  engine_version         = "14"
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.demo.name
